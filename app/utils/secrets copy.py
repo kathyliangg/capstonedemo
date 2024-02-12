baaccabd6954588190e14e39c4daf88948a62b06
kathyliangg <beanieebutt@gmail.com>
2024-02-12 11:24:35 -0800

    updated secrets

--- app/utils/secrets copy.py
+++ app/utils/secrets copy.py
@@ -2,8 +2,8 @@ import os
 
 def getSecrets():
     secrets = {
-        'MONGO_HOST':"",
-        'MONGO_DB_NAME':"",
+        'MONGO_HOST':"mongodb+srv://lattekat:343305@lattekat.azra2tt.mongodb.net/kathypractice?retryWrites=true&w=majority",
+        'MONGO_DB_NAME':"kathypractice",
         'GOOGLE_CLIENT_ID': "",
         'GOOGLE_CLIENT_SECRET':"",
         'GOOGLE_DISCOVERY_URL':"https://accounts.google.com/.well-known/openid-configuration",
